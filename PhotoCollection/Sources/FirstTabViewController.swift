5cf0b07cb8c46230e7b52470c1240a429192f90a
Artyom <artyomka13@gmail.com>
2022-04-27 16:45:35 +0300

    add firstTabViewController

--- PhotoCollection/Sources/FirstTabViewController.swift
+++ PhotoCollection/Sources/FirstTabViewController.swift
@@ -5,4 +5,14 @@
 //  Created by Artyom Guzenko on 27.04.2022.
 //
 
-import Foundation
+import UIKit
+
+class FirstTabViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Медиатека"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view.backgroundColor = .systemGray6
+    }
+}

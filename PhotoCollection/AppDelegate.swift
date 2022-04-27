44216f60b8e5af77e43782c97382421b62a35168
Artyom <artyomka13@gmail.com>
2022-04-27 16:32:26 +0300

    add tabBarController

--- PhotoCollection/AppDelegate.swift
+++ PhotoCollection/AppDelegate.swift
@@ -9,28 +9,37 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let tabBarController = UITabBarController()
+        
+        let firstTabViewController = FirstTabViewController()
+        let firstNavigationController = UINavigationController(rootViewController: firstTabViewController)
+        firstTabViewController.tabBarItem = UITabBarItem(title: "Медиатека", image: UIImage(named: "photo"), tag: 0)
+        
+        let secondTabViewController = SecondTabViewController()
+        let secondNavigationController = UINavigationController(rootViewController: secondTabViewController)
+        secondTabViewController.tabBarItem = UITabBarItem(title: "Для Вас", image: UIImage(named: "custom.heart"), tag: 1)
+
+        let thirdTabViewController = ThirdTabViewController()
+        let thirdNavigationController = UINavigationController(rootViewController: thirdTabViewController)
+        thirdTabViewController.tabBarItem = UITabBarItem(title: "Альбомы", image: UIImage(named: "custom.square"), tag: 2)
+        
+        let fourthTabViewController = FourthTabViewController()
+        let fourthNavigationController = UINavigationController(rootViewController: fourthTabViewController)
+        fourthTabViewController.tabBarItem = UITabBarItem(title: "Поиск", image: UIImage(named: "custom.magnifyin"), tag: 3)
+        tabBarController.setViewControllers([firstNavigationController,
+                                             secondNavigationController,
+                                             thirdNavigationController,
+                                             fourthNavigationController], animated: true)
+        tabBarController.selectedIndex = 2
+        
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
-
